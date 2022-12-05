aa661defe2d524322e22a76f17e0955112e49940
Aidan <ah65496p@pace.edu>
2022-12-05 14:02:46 -0500

    lab9

--- assets/scripts.js
+++ assets/scripts.js
@@ -29,11 +29,29 @@ console.log(itemCount)
     currentPrice = currentPrice + 25
    }
 
+   document.querySelector(".hoverText").innerHTML = currentPrice
+   let checkout = reset(currentPrice)
+
    console.log(currentPrice) 
 }
 
-function checkout() {
+let checkout = prompt("Please leave your name and address for billing purposes.")
+
+ {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    window.alert(`You have a total of ${itemCount} items that will cost $${currentPrice}. Thank you for shopping with us`)
+    //window.alert(`You have a total of ${itemCount} items that will cost $${currentPrice}. Thank you for shopping with us`)
+    window.prompt("Please provide your name and address. /n Thank you for shopping with us!")
+
+}
+
+let changableParagraph = document.getElementById("cartItems")
+
+function darkMode() {
+	// add your code here
+    
+    document.querySelector("body").style.backgroundColor = "black"
+    document.querySelector("main").style.color = "white"
+    document.querySelector("h1").style.color = "white"
+
 }
